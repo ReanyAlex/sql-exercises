90f9945c612b93ab3303c00acb23aef8fa3689b1
Alex Reany <reanyalex@gmail.com>
2017-05-04 16:09:47 -0700

    small improv

--- sql/09-mutual-likes.sql
+++ sql/09-mutual-likes.sql
@@ -3,14 +3,22 @@
 Write the SQL in the file sql/09-mutual-likes.sql
 */
 
-SELECT  a.name AS liker_name, a.id
-FROM exercises.student_like
-JOIN exercises.students a
-ON a.id = student_like.liker_id
+-- UNNECESSARILY COMPLICATED QUERY
+SELECT name, id_result FROM exercises.students
+JOIN
+(SELECT liker_id AS id_result FROM (
+SELECT  a.liker_id  FROM exercises.students
+JOIN exercises.student_like a
+ON students.id = a.liker_id
+JOIN exercises.student_like b
+ON students.id = b.likee_id
+) AS test1
 INTERSECT
-SELECT  b.name AS liker_name, b.id
-FROM exercises.student_like
-JOIN exercises.students b
-ON b.id = student_like.likee_id
-
--- extra name need to be fixed
+SELECT likee_id AS id_result FROM (
+SELECT  a.likee_id FROM exercises.students
+JOIN exercises.student_like a
+ON students.id = a.liker_id
+JOIN exercises.student_like b
+ON students.id = b.likee_id
+) AS test1) test98
+ON students.id = test98.id_result;
